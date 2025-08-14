1a263b021cbc01563d587392071608c48a7826e6
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-14 11:08:29 +0700

    Enhance interval subscription with RxJS operators for value transformation
    - pipe allow you to add operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
-import { interval } from 'rxjs';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,10 @@ export class AppComponent implements OnInit    {
   private destroyRef = inject(DestroyRef);
   
   ngOnInit() {
-  const subscribtion = interval(1000).subscribe({
+  const subscribtion = interval(1000).pipe(
+    // You can add operators here
+    map(value => value * 2) // Example operator to double the emitted value
+  ).subscribe({
       next: (value) => {
         console.log('Interval value:', value);
       },
