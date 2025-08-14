a1e6d3b99d09a0144944388a7edf5d1357656bc8
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-08-14 10:46:36 +0700

    Implement OnInit lifecycle hook to log interval values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit    {
+  ngOnInit() {
+    interval(1000).subscribe({
+      next: (value) => {
+        console.log('Interval value:', value);
+      },
+      error: (err) => {
+        console.error('Error occurred:', err);
+      },
+      complete: () => {
+        // This will never be called since interval is infinite
+        console.log('Interval completed');
+      }
+    });
+  }
 
 }
